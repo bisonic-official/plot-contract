26fa192ce0e0adecc8f420a54129dc1a62a8684b
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2024-07-10 18:56:28 -0600

    Adds deployment for Ronin.

---
+++ deploy/deploy.js
@@ -0,0 +1,32 @@
+const func = async function (hre) {
+    const { deployments, getNamedAccounts } = hre;
+    const { deploy } = deployments;
+
+    const { deployer } = await getNamedAccounts();
+    console.log("Deploying contracts with the account:", deployer);
+
+    try {
+        const runiverseLandContract = await deploy('RuniverseLand', {
+            from: deployer,
+            args: [
+                "https://api.runiverse.world/GetPlotInfo?PlotId="
+            ],
+            log: true,
+            gasPrice: 30000000000
+        });
+
+        const runiverseLandMinterContract = await deploy('RuniverseLandMinter', {
+            from: deployer,
+            args: [
+                runiverseLandContract.address
+            ],
+            log: true,
+            gasPrice: 30000000000
+        });
+    } catch (error) {
+        console.error("Error deploying contracts:", error);
+    }
+};
+
+module.exports = func;
+func.tags = ['RuniverseLand'];
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('solidity-coverage');
+require('hardhat-deploy');
 
 // Go to https://www.alchemyapi.io, sign up, create
 // a new App in its dashboard, and replace "KEY" with its key
@@ -13,7 +14,18 @@ const SEPOLIA_PRIVATE_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  namedAccounts: {
+    deployer: 'privatekey://XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+  },
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
@@ -22,6 +34,16 @@ module.exports = {
     goerli: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [SEPOLIA_PRIVATE_KEY]
+    },
+    ronin: {
+      chainId: 2020,
+      url: 'https://api.roninchain.com/rpc',
+      gasPrice: 20_000_000_000,
+    },
+    saigon: {
+      chainId: 2021,
+      url: 'https://saigon-testnet.roninchain.com/rpc',
+      gasPrice: 20_000_000_000,
     }
   }
 };
